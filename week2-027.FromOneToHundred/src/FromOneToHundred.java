5ce99be9dabc11e57b6b6c61f7bf819defe3c3e4
PrasannaBH <55347026+PrasannaBH@users.noreply.github.com>
2020-01-26 19:34:43 +0530

    Complete FromOneToHundred

---
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,16 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        // Write your program here
+
+        int num = 1;
+
+        while(num < 101){
+
+            System.out.println(""+num);
+
+            num++;
+        }
+    }
+}
