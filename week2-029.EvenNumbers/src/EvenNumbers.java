04b7492f2d6d41cd287e77d67a084b7d189c442f
PrasannaBH <55347026+PrasannaBH@users.noreply.github.com>
2020-01-26 19:37:10 +0530

    Complete EvenNumbers

---
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -0,0 +1,17 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        // Write your code here
+
+        int num = 2;
+
+        while(num < 101){
+
+            if (num % 2 == 0)
+                System.out.println(""+num);
+
+            num++;
+        }
+    }
+}
